5e329f64deb69f2523f98551b1bf63a986b96d6b
timsherwood <sherwood@cs.ucsb.edu>
2014-07-05 14:03:43 -0700

    run_test as direct script

--- test/adder.py
+++ test/adder.py
@@ -27,17 +27,15 @@ def run_adder():
     r = rtl.Register(bitwidth=bitwidth,name='r')
     r.next, cout = add(r, rtl.Const(1).zero_extended(bitwidth) )
 
-#-----------------------------------------------------
+    sim_trace = rtl.SimulationTrace()
+    on_reset = {} # signal states to be set when reset is asserted
 
-sim_trace = rtl.SimulationTrace()
-on_reset = {} # signal states to be set when reset is asserted
+    # build the actual simulation environment
+    sim = rtl.Simulation( register_value_map=on_reset, default_value=0, tracer=sim_trace )
 
-# build the actual simulation environment
-sim = rtl.Simulation( register_value_map=on_reset, default_value=0, tracer=sim_trace )
-
-# step through 15 cycles
-for i in xrange(15):  
-    sim.step( {} )
-
-#sim_trace.render_trace()
-return sim_trace
+    # step through 15 cycles
+    for i in xrange(15):  
+        sim.step( {} )
+        
+    #sim_trace.render_trace()
+    return sim_trace
