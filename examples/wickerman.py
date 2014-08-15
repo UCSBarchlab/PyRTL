5674f6514afc539fd72157c07abe8c390f245f0e
timsherwood <sherwood@cs.ucsb.edu>
2014-08-15 14:09:22 -0700

    alus added

--- examples/wickerman.py
+++ examples/wickerman.py
@@ -7,8 +7,29 @@ def wickerman_core( bitwidth, addrwidth ):
     dmem = MemBlock(bitwidth, addrwidth, 'dmem')
     regfile = MemBlock(bitwidth, 5, 'imem')
     pc = Register(addrwidth,'pc')
-                
-                
+
+    # decode the current instruction
+    instr = imem[pc]
+    opcode = instr[26:]
+    immed = instr[10:26]
+    rt = instr[10:15]
+    rs = instr[5:10]
+    rd = instr[0:5]
+
+def alu( command, a, b )
+    """ perform command on a and b, return tuple of value and status """
+    sub_r = a - b
+    add_r = a + b
+    mul_r = a * b
+    and_r = a & b
+    or_r  = a | b
+    le_r  = a < b
+
+    if command==add
+    elif command==op
+    elif command==le
+    
+
 wickerman_core(5,5)
                 
 # Simulation of the core
