8e263302aa0ab08090258b8c72a6454939d3c824
timsherwood <sherwood@cs.ucsb.edu>
2014-08-16 23:48:14 -0700

    formatting

--- examples/wickerman.py
+++ examples/wickerman.py
@@ -1,12 +1,13 @@
 import sys
-sys.path.append("..") # needed only if not installed
+sys.path.append("..")  # needed only if not installed
 from pyrtl import *
 
-def wickerman_core( bitwidth, addrwidth ):
+
+def wickerman_core(bitwidth, addrwidth):
     imem = MemBlock(bitwidth, addrwidth, 'imem')
     dmem = MemBlock(bitwidth, addrwidth, 'dmem')
     regfile = MemBlock(bitwidth, 5, 'imem')
-    pc = Register(addrwidth,'pc')
+    pc = Register(addrwidth, 'pc')
 
     # decode the current instruction
     instr = imem[pc]
@@ -16,25 +17,26 @@ def wickerman_core( bitwidth, addrwidth ):
     rs = instr[5:10]
     rd = instr[0:5]
 
-def alu( command, a, b )
+
+def alu(command, a, b):
     """ perform command on a and b, return tuple of value and status """
     sub_r = a - b
     add_r = a + b
     mul_r = a * b
     and_r = a & b
-    or_r  = a | b
-    le_r  = a < b
+    or_r = a | b
+    le_r = a < b
 
-    if command==add
-    elif command==op
-    elif command==le
-    
+    #if command == add
+    #elif command == op
+    #elif command == le
 
-wickerman_core(5,5)
-                
+
+wickerman_core(5, 5)
+
 # Simulation of the core
 sim_trace = SimulationTrace()
-sim = Simulation( tracer=sim_trace )
+sim = Simulation(tracer=sim_trace)
 for i in xrange(15):
-    sim.step( {} )
+    sim.step({})
 sim_trace.render_trace()
