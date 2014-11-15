80f713d7bb6106ae604fbecc90fe95ef7ce86f79
Joseph McMahan <jemcmahan13@gmail.com>
2014-11-14 18:51:00 -0800

    Design laid out on paper. pyrtl evolving.

--- research/hmachine/hm.py
+++ research/hmachine/hm.py
@@ -5,57 +5,105 @@ import pyrtl
 from pyrtl import *
 
 # Machine Parameters
-nArgs = 8
-lognargs = int(np.log2(nArgs))
-nLocals = 16
-width = 32
+argspace = 3  # number of bits to specify arg number
+nArgs = pow(2, argspace)  # maximum number of function arguments
+localspace = 4  # number of bits to specify local var number
+nLocals = pow(2, localspace)  # maximum number of local vars in a function
+freevarspace = 8  # number of bits to specify free variable number
+nfreevars = pow(2, freevarspace)  # maximum number of free variables
+width = 32  # machine width
+
+# Memory sizes
+namespace = 10  # number of bits used in IDs
+ntablesize = pow(2, namespace)  # possible number of IDs (currently just 1 memory block)
+evalstackspace = 15  # number of bits in eval stack addresses
+evalstacksize = pow(2, evalstackspace)
+heapspace = 16  # number of bits in heap addresses
+heapsize = pow(2, heapspace)  # number of words in heap
+textspace = 15  # number of bits in text memory addresses (a.k.a. immortal heap)
+textsize = pow(2, textspace)  # number of words of immortal heap memory
 
 def main():
-    # ######################################################################
-    #     Instruction Decode
-    # ######################################################################
+
+    # Registers
+    PC = register(textspace, "ExecutionPointer")  # addr of next instr
+    envclo = register(namespace, "CurrentClosure")  # name of closure we're in now
+    nlocalsreg = register(localspace, "NumberLocals")  # number of locals bound so far
+    nargsreg = register(argspace, "NumberArgs")  # number of args bound so far
+    nfreevarreg = register(freevarspace, "NumberFreeVars")  # number of freevars bound so far
+    hp = register(heapspace, "HeapPointer")  # heap pointer (next free addr)
+    sp = register(svalstackspace "EvalStackPointer")  # evaluation stack pointer
+    rr = register(width, "ReturnRegister")  # return register
+
+    # Argument Registers
+    argR = WireVector(width, "argsReadData")
+    argW = WireVector(width, "argsWriteData")
+    argwe = WireVector(1, "argsWriteEnable")
+    argRaddr = WireVector(argspace, "argsReadAddr")
+    argWaddr = WireVector(argspace, "argsWriteAddr")
+    argWaddr <<= nargsreg  # write to next free arg reg
+    argSwitch = WireVector(1, "argsSwitch")
+    arg1 = WireVector(width, "argreg1")
+    arg2 = WireVector(width, "argreg2")
+    argregs(argwe, argsWaddr, argW, argRaddr, argR, argSwitch, arg1, arg2)
+
+# ######################################################################
+#     Instruction Decode
+# ######################################################################
 
 
-    # ######################################################################
-    #     Argument Registers
-    # ######################################################################
-
+# ######################################################################
+#     Argument Registers
+# ######################################################################
+def argregs(we, waddr, wdata, raddr, rdata, flipstate, reg1, reg2):
     # Two banks of registers, one bit keeping track of which is read vs write
-    # One reg tracks number of args written
-
-    # Input/Output
-    write = Input(1, 'write')  # write reg number is tracked internally
-    writedata = Input(width, 'writedata')
-    writeaddr = Input(lognargs, 'writeaddr')
-    readaddr = Input(lognargs, 'readaddr')
-    readdata = Output(width, 'readdata')
-    flipstate = Input(1, 'flipargstate')
 
     # Handle I/O based on internal state
     state = Register(1, 'argstate')
-
-    # Register banks
     # In each state, one is read and one is written; on flip, write becomes read and
     #  read is cleared
     # state == 0: args1 is writeargs, args2 is readargs
     # state == 1: args1 is readargs, args2 is writeargs
-    #help(wire)
-    read1 = WireVector(width, "argsbank1")
-    read2 = WireVector(width, "argsbank2")
-    
-    args1 = RegisterFile(width, nArgs, writeaddr, writedata, readaddr, read1, 
-                         (state == 0) & write, (state == 1) & flipstate, name='args1-')
-    args2 = RegisterFile(width, nArgs, writeaddr, writedata, readaddr, read2, 
-                         (state == 1) & write, (state == 0) & flipstate, name='args2-')
-    # mux for output
-    readdata <<= mux(state, falsecase=read2, truecase=read1)
+
+    args1 = MemBlock(width, argspace, name="args1")
+    args2 = MemBlock(width, argspace, name="args2")
+
+    # Output
+    #read1 = WireVector(width, "args1read")
+    #read2 = WireVector(width, "args2read")
+    read1 = args1[raddr]
+    read2 = args2[raddr]
+    rdata <<= mux(state, falsecase=read2, truecase=read1)  # mux for output
+    # Additional ports to output arg0 and arg1; need for primitive (ALU) ops
+    reg1 <<= mux(state, falsecase=args2[0], truecase=args1[0])
+    reg2 <<= mux(state, falsecase=args2[1], truecase=args1[1])
+
+    # Input
+    c = ConditionalUpdate()
+    with c(we & (state == 0)):
+        args1[waddr] = wdata
+    with c(we & (state == 1)):
+        args2[waddr] = wdata
     
     # Handle state flips
-    c = ConditionalUpdate()
     with c(flipstate):
         state.next <<= ~state
         
     pyrtl.working_block().sanity_check()
+
+# ######################################################################
+#     Primitive Ops Unit (a.k.a. ALU)
+# ######################################################################
+
+
+
+
+
+
+
+
+
+
 
 
 class RegisterFile:
