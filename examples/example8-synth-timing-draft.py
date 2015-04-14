c3366bd41156961027f300e73ac5cd3103e5d78c
John Clow <johnwclow@hotmail.com>
2015-04-13 19:12:03 -0700

    Added draft of the synth and timing example

---
+++ examples/example8-synth-timing-draft.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.extend(['C:\\Users\\John\\Documents\\PycharmProjects\\PyRTL'])
+
+import pyrtl
+import pyrtl.core
+import pyrtl.wire
+import pyrtl.passes
+
+pyrtl.reset_working_block()
+constwire = pyrtl.Const(6, bitwidth=4)
+inwire2 = pyrtl.Input(bitwidth=4, name="input2")
+outwire = pyrtl.Output(bitwidth=5, name="output")
+
+outwire <<= constwire + inwire2
+
+timing_map = pyrtl.timing_analysis()
+print "Pre Synthisis:"
+pyrtl.print_max_length(timing_map)
+print ""
+
+block_max_time = pyrtl.passes.timing_max_length(timing_map)
+# tempxx = pyrtl.passes.timing_critical_path(timing_map)
+
+
+#
+# Synthesis
+#
+
+pyrtl.synthesize()
+timing_map = pyrtl.timing_analysis()
+print "Pre Optimization:"
+pyrtl.print_max_length(timing_map)
+print ""
+for net in pyrtl.working_block().logic:
+    print str(net)
+
+
+#
+# Optimization
+#
+
+# tempxx = pyrtl.passes.timing_critical_path(timing_map)
+
+pyrtl.optimize()
+
+timing_map = pyrtl.timing_analysis()
+print "Post Optimization:"
+pyrtl.print_max_length(timing_map)
+print ""
+for net in pyrtl.working_block().logic:
+    print str(net)
+
+block_max_time = pyrtl.passes.timing_max_length(timing_map)
+tempxx = pyrtl.passes.timing_critical_path(timing_map)
