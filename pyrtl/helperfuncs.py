1b0f0ac4137dc3f0b9353d8557193ce291cd7890
John Clow <johnwclow@hotmail.com>
2015-04-17 16:07:20 -0700

    Moving the match bitwidth function to core (justification: a variant of this function is often being used in core

--- pyrtl/helperfuncs.py
+++ pyrtl/helperfuncs.py
@@ -149,3 +149,14 @@ def concat(*args):
             dests=(outwire,))
         outwire.block.add_net(net)
         return outwire
+
+
+def match_bitwidth(*args):
+    # TODO: allow for custom bit extension functions
+    """
+    Matches the bitwidth of all of the input arguments
+    :type args: WireVector
+    :return tuple of args in order with extended bits
+    """
+    max_len = max(len(wv) for wv in args)
+    return (wv.zero_extended(max_len) for wv in args)
--- rtllib/libutils.py
+++ rtllib/libutils.py
@@ -1,3 +1,4 @@
+import pyrtl
 from pyrtl import *
 
 
@@ -8,5 +9,4 @@ def match_bitwidth(*args):
     :type args: WireVector
     :return tuple of args in order with extended bits
     """
-    max_len = max(len(wv) for wv in args)
-    return (wv.zero_extended(max_len) for wv in args)
+    return pyrtl.match_bitwidth(*args)
