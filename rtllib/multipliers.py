c759e3d0c4e0f792e897d01ca4e67cdd92b65b23
Britt Christy <iamburitto@gmail.com>
2015-04-29 13:49:10 -0700

    Added extended euclidean gcd solver code. Next step: PyRTL-ize it.

--- rtllib/multipliers.py
+++ rtllib/multipliers.py
@@ -230,6 +230,21 @@ def test_wallace_tree():
     sim_trace.render_trace()
 
 
+def extended_gcd(aa, bb):
+    lastremainder, remainder = abs(aa), abs(bb)
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient*x, x
+        y, lasty = lasty - quotient*y, y
+    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+ 
+def modinv(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g != 1:
+        raise ValueError
+    return x % m
+
 def another_montgomery_multiplier(A,B,N):
     
     assert len(A) == len(B) == len(N)
@@ -295,6 +310,9 @@ def test_modulus():
 
 
     modded <<= another_montgomery_multiplier(a, b, n)
+
+    print modinv(16, 11)
+
     # A B N  Magic 
     # 3 7 5  1
     # A Magic N => x
