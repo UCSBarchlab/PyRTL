7f6b4b2ecb544e520f79070e3ff98724db7f2c86
John Clow <johnwclow@hotmail.com>
2015-06-30 18:40:52 -0700

    Made a few small additional updates to the barrel shifter documentation

--- rtllib/barrel.py
+++ rtllib/barrel.py
@@ -4,7 +4,7 @@ from pyrtl import *
 import math
 
 
-def barrel_shifter(shift_in, bit_in, direction, shift_dist):
+def barrel_shifter(shift_in, bit_in, direction, shift_dist, wrap_around=0):
     """
     Create a barrel shifter that operates on data based on the wire width
     :param shift_in:the input wire;
@@ -12,19 +12,20 @@ def barrel_shifter(shift_in, bit_in, direction, shift_dist):
     :param direction: direction is a one bit wirevector representing shift direction
         1 = shift down, 0 = shift up.
     :param shift_dist: wirevector representing offset to shift
+    :param wrap_around: ****currently not implemented*****
     :return: shifted wirevector
     """
     # Implement with logN stages muxing between shifted and un-shifted values
 
     val = shift_in
     appendval = bit_in
-    log_length = int(math.log(len(shift_in)-1, 2))
+    log_length = int(math.log(len(shift_in)-1, 2))  # note the one offset
 
     if len(shift_dist) > log_length:
         print "Warning: for barrel shifter, the shift distance wirevector " \
               "has bits that are not used in the barrel shifter"
 
-    for i in range(min(len(shift_dist), log_length)):  # note the one offset
+    for i in range(min(len(shift_dist), log_length)):
         shift_amt = pow(2, i)  # stages shift 1,2,4,8,...
         newval = mux(direction, truecase=val[:-shift_amt], falsecase=val[shift_amt:])
         newval = mux(direction, truecase=concat(newval, appendval),
