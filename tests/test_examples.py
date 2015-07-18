3d306970cc5093b2d1802aa45ce904a83527ad09
John Clow <johnwclow@hotmail.com>
2015-07-17 18:46:48 -0700

    We still want the code to print the subprocess to stdout

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -26,6 +26,6 @@ def example_t(file):
     print "testing file: " + file
     pyrtl.reset_working_block()
     try:
-        output = subprocess.check_output(['python', file])
+        subprocess.check_call(['python', file])
     except subprocess.CalledProcessError, e:
         raise e
