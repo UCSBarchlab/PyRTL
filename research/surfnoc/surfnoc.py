516c759c15d435bbac13df98cbf862d13024a8c1
Bharath Shashidhar <bharathshashidhar@umail.ucsb.edu>
2015-07-17 17:50:27 -0700

    Add surfnoc.py

--- research/surfnoc/surfnoc.py
+++ research/surfnoc/surfnoc.py
@@ -1,1 +1,27 @@
-# add me here
+import pyrtl
+
+
+class surf_noc(object):
+    """This class defines individual router. It should be instantiated to use in 4*4 torus
+    Router as 1.Buffer 2.Route Computation Unit(RC) 3.Virtual Channel Allocator(VC) 4.Switch Allocator (SA) 5. Crossbar
+
+    """
+    def __init__(self, routerid):
+	"""Returns a router with id = routerid"""
+	self.routerid=routerid
+     
+    def buffer():
+	
+
+    def route_computer():
+
+
+    def vc_allocator():
+
+
+
+    def sw_allocator():
+
+
+
+    def crossbar():
