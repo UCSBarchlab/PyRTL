47c623775d7eefa320947209814ef8d3c06eea6e
timsherwood <sherwood@cs.ucsb.edu>
2015-09-04 23:49:38 -0700

    huge conditional stress test added to conditional

--- tests/test_conditional.py
+++ tests/test_conditional.py
@@ -358,7 +358,6 @@ class TestWireConditionalBlock(unittest.TestCase):
 
 # ---------------------------------------------------------------
 
-""" 
 class TestNonExclusiveBlocks(unittest.TestCase):
     def setUp(self):
         pyrtl.reset_working_block()
@@ -387,7 +386,7 @@ class TestNonExclusiveBlocks(unittest.TestCase):
             with pyrtl.otherwise: pass
             with r2 < 3:
                 r2.next |= r2 + 1
-        self.check_trace('i 01230123\nr 00024445\n')
+        self.check_trace(' i 01230123\nr1 01233333\nr2 01233333\n')
 
     def test_one_deep_nested_non_exclusive_condition(self):
         i = pyrtl.Register(bitwidth=2, name='i')
@@ -401,14 +400,15 @@ class TestNonExclusiveBlocks(unittest.TestCase):
                 with pyrtl.otherwise: pass
                 with r2 < 3:
                     r2.next |= r2 + 2
-        self.check_trace('i 01230123\nr 00024445\n')
+        self.check_trace(' i 01230123\nr1 00024444\nr2 00024444\n')
 
+""" 
     def test_overlaping_assignments_in_non_exclusive_assignments(self):
         i = pyrtl.Register(bitwidth=2, name='i')
         i.next <<= i + 1
         r1 = pyrtl.Register(bitwidth=3, name='r1')
         r2 = pyrtl.Register(bitwidth=3, name='r2')
-        with assertRaises(pyrtl.PyrtlError):
+        with self.assertRaises(pyrtl.PyrtlError):
             with pyrtl.conditional_assignment:
                 with r1 < 3:
                     r1.next |= r1 + 1
@@ -417,6 +417,110 @@ class TestNonExclusiveBlocks(unittest.TestCase):
                     r1.next |= r2 + 1
         self.check_trace('i 01230123\nr 00024445\n')
 """
+
+class TestWireConditionalBlock(unittest.TestCase):
+    def setUp(self):
+        pyrtl.reset_working_block()
+
+    def tearDown(self):
+        pyrtl.reset_working_block()
+
+    def test_super_stress_test(self):
+        a,b,x,y,i,j,k,m,z,c,d = [pyrtl.Input(1,n) for n in 'abxyijkmzcd']
+        allin = [a,b,x,y,i,j,k,m,z,c,d]
+        var0,var1,var2,var3,var4 = [pyrtl.Output(4,'var'+str(index)) for index in range(5)]
+        allout = [var0,var1,var2,var3]
+
+        """ 
+         1  with a:  # a
+         2  with b:  # not(a) and b
+         3      with x:  # not(a) and b and x
+         4      with otherwise:  # not(a) and b and not(x)
+         5      with y:  # not(a) and b and y;  check(3,4)
+         6          with i:  # not(a) and b and y and i;  check(3,4)
+         7          with j:  # not(a) and b and y and not(i) and j;  check(3,4)
+         8          with otherwise:  # not(a) and b and y and not(i) and not(j):  check(3,4)
+         9          with k:  # not(a) and b and y and k;  check(3,4,6,7,8)
+         8          with otherwise:  # not(a) and b and y and not(k):  check(?)
+        10          with m:  # not(a) and b and y and m;  check(?)
+        11  with otherwise:  #not(a) and not(b)     
+        12      with z: #not(a) and not(b) and z
+        13  with c:  #c;  check(1,2,3,4,5,6,7,8,9,10,11,12)
+        14  with d:  #not(c) and d;  check(1,2,3,4,5,6,7,8,9,10,11,12)
+        """
+
+        with pyrtl.conditional_assignment:
+            with a:
+                var0 |= 1
+            with b:
+                with x:
+                    var0 |= 2
+                with pyrtl.otherwise:
+                    var0 |= 3
+
+                with y:
+                    with i:
+                        var1 |= 1
+                    with j:
+                        var1 |= 2
+                    with pyrtl.otherwise:
+                        var1 |= 3
+
+                    with k:
+                        var2 |= 1
+                    with pyrtl.otherwise:
+                        var2 |= 2
+
+                    with m:
+                        var3 |= 3
+
+            with pyrtl.otherwise:
+                with z:
+                    var0 |= 4
+
+            with c:
+                var4 |= 1
+            with d:
+                var4 |= 2
+
+
+        sim_trace = pyrtl.SimulationTrace()
+        sim = pyrtl.Simulation(tracer=sim_trace)
+        for cycle in xrange(2**len(allin)):
+            inputs = {v:0x1&(cycle>>i) for i, v in enumerate(allin[::-1])}
+            sim.step(inputs)
+
+        for cycle in range(len(sim_trace)):
+            def v(var):
+                return sim_trace.trace[var][cycle]
+            if v(a):
+                self.assertTrue(v(var0)==1)
+            elif v(b):
+                if v(x):
+                    self.assertTrue(v(var0)==2)
+                else:
+                    self.assertTrue(v(var0)==3)
+                if v(y):
+                    if v(i):
+                        self.assertTrue(v(var1)==1)
+                    elif v(j):
+                        self.assertTrue(v(var1)==2)
+                    else:
+                        self.assertTrue(v(var1)==3)
+                    if v(k):
+                        self.assertTrue(v(var2)==1)
+                    else:
+                        self.assertTrue(v(var2)==2)
+                    if v(m):
+                        self.assertTrue(v(var3)==3)
+            else:
+                if v(z):
+                    self.assertTrue(v(var0)==4)
+            if v(c):
+                self.assertTrue(v(var4)==1)
+            elif v(d):
+                self.assertTrue(v(var4)==2)
+
 
 if __name__ == "__main__":
     unittest.main()
