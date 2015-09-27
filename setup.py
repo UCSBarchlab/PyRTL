cc69d10aa195d3b8ac07e7cd5eb1ab1412c2b41f
timsherwood <sherwood@cs.ucsb.edu>
2015-09-27 16:40:15 -0700

    setup for cheeseshop

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup
 
-config = {
-    'name': 'pyrtl',
-    'description': 'A RTL-level hardware design and simulation toolkit.',
-    'author': 'Timothy Sherwood, John Clow, and the UCSBarchlab',
-    'url': 'http://ucsbarchlab.github.io/PyRTL/',
-    'download_url': 'https://github.com/UCSBarchlab/PyRTL/tarball/master',
-    'author_email': 'sherwood@cs.ucsb.edu',
-    'version': '0.8',
-    'install_requires': ['pyparsing'],
-    'packages': ['pyrtl'],
-    'scripts': []
-}
-
-setup(**config)
+setup(
+    name = 'pyrtl',
+    version = '0.8.0',
+    packages =  ['pyrtl'],
+    description = 'RTL-level Hardware Design and Simulation Toolkit',
+    author =  'Timothy Sherwood, John Clow, and UCSBarchlab',
+    author_email =  'sherwood@cs.ucsb.edu',
+    url =  'http://ucsbarchlab.github.io/PyRTL/',
+    download_url = 'https://github.com/UCSBarchlab/PyRTL/tarball/0.8.0',
+    install_requires =  ['six'],
+    test_requires =  ['tox','nose'],
+    extras_requires =  ['pyparsing'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: System :: Hardware']
+)
