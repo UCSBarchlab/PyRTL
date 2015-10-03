b797f046fbaa2cc6b63a3ca9c2e66756f7bc9f7e
John Clow <johnwclow@hotmail.com>
2015-10-03 10:29:11 -0700

    renaming variables to reflect that str_to_int_array does not just use hex strings

--- pyrtl/rtllib/strlib.py
+++ pyrtl/rtllib/strlib.py
@@ -14,5 +14,5 @@ def str_to_int_array(string, base=16):
     "13 a3 3c" => [0x13, 0xa3, 0x3c]
     """
 
-    hex_strings = string.split()
-    return [int(hex_str, base) for hex_str in hex_strings]
+    int_strings = string.split()
+    return [int(int_str, base) for int_str in int_strings]
