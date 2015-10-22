36f21424ada4b7dcd00186ac472f76bf528807df
John Clow <johnwclow@hotmail.com>
2015-10-22 11:50:58 -0700

    More adder cleanup

--- pyrtl/rtllib/adders.py
+++ pyrtl/rtllib/adders.py
@@ -52,6 +52,13 @@ def _one_bit_add_no_concat(a, b, cin):
     return cout, sum
 
 
+def half_adder(a, b):
+    assert len(a) == len(b) == 1
+    sum = a ^ b
+    cout = a & b
+    return cout, sum
+
+
 def ripple_add(a, b, cin=0):
     a, b = libutils.match_bitwidth(a, b)
     cin = pyrtl.as_wires(cin)
@@ -63,17 +70,18 @@ def ripple_add(a, b, cin=0):
         return pyrtl.concat(msbits, ripplecarry[0])
 
 
-def carrysave_adder(a, b, c):
+def carrysave_adder(a, b, c, final_adder=ripple_add):
     """
     Adds three wirevectors up in an efficient manner
-    :param a, b, c: the three wirevectors to add up
+    :param a, b, c wirevector: the three wires to add up
+    :param final_adder function: The adder to use to do the final addition
     :return: a wirevector with length 2 longer than the largest input
     """
-    libutils.match_bitwidth(a, b, c)
+    a, b, c = libutils.match_bitwidth(a, b, c)
     partial_sum = a ^ b ^ c
     shift_carry = (a | b) & (a | c) & (b | c)
     shift_carry_1 = pyrtl.concat(shift_carry, 0)
-    return ripple_add(partial_sum, shift_carry_1)
+    return final_adder(partial_sum, shift_carry_1)
 
 
 def wallace_reducer(wire_array_2, result_bitwidth, final_adder=kogge_stone):
@@ -119,15 +127,6 @@ def _general_adder_reducer(wire_array_2, result_bitwidth, reduce_2s, final_adder
     :return: wirevector of length result_wirevector
     """
     # verification that the wires are actually wirevectors of length 1
-    """
-
-    These reductions take place in the form of full-adders (3 inputs),
-    half-adders (2 inputs), or just passing a wire along (1 input).
-
-    These reductions take place as long as there are more than 2 wire
-    vectors left. When there are 2 wire vectors left, you simply run the
-    2 wire vectors through a Kogge-Stone adder.
-    """
     for wire_set in wire_array_2:
         for a_wire in wire_set:
             if not isinstance(a_wire, pyrtl.WireVector) or len(a_wire) != 1:
@@ -138,59 +137,52 @@ def _general_adder_reducer(wire_array_2, result_bitwidth, reduce_2s, final_adder
     while not all(len(i) <= 2 for i in wire_array_2):
         deferred = [[] for weight in range(result_bitwidth)]
         for i, w_array in enumerate(wire_array_2):  # Start with low weights and start reducing
-            while len(w_array) >= 3:  # Reduce with Full Adders until < 3 wires
+            while len(w_array) >= 3:
                 cout, sum = _one_bit_add_no_concat(*(w_array.pop(0) for j in range(3)))
-                deferred[i].append(sum)  # deferred bit keeps this sum
-                if i + 1 < result_bitwidth:  # watch out for index bounds
-                    deferred[i + 1].append(cout)  # cout goes up by one
-
-            if len(wire_array_2[i]) == 2 and reduce_2s:
-                # Reduce with a Half Adder if exactly 2 wires remain
-                a, b = w_array.pop(0), w_array.pop(0)
-                deferred[i].append(a ^ b)  # deferred bit keeps this sum
+                deferred[i].append(sum)
                 if i + 1 < result_bitwidth:
-                    deferred[i + 1].append(a & b)  # cout goes up one weight
+                    deferred[i + 1].append(cout)
+
+            if len(w_array) == 2 and reduce_2s:
+                cout, sum = half_adder(*w_array)
+                deferred[i].append(sum)
+                if i + 1 < result_bitwidth:
+                    deferred[i + 1].append(cout)
             else:
                 deferred[i].extend(w_array)
 
-        wire_array_2 = deferred  # Set bits equal to the deferred values
+        wire_array_2 = deferred
 
     # At this stage in the multiplication we have only 2 wire vectors left.
-    add_wires = [], []
-    result = None
-
-    for i in range(result_bitwidth):
-        if len(wire_array_2[i]) == 2:  # Check if the two wire vectors overlap yet
-            break
-        if result is None:
-            result = wire_array_2[i][0]
-        else:
-            result = pyrtl.concat(wire_array_2[i][0], result)
-
-    for j in range(i, result_bitwidth):
-        for i in range(2):
-            if len(wire_array_2[j]) >= i + 1:
-                add_wires[i].insert(0, wire_array_2[j][i])
-            else:
-                add_wires[i].insert(0, pyrtl.Const(0))
-
-    adder_result = final_adder(pyrtl.concat(*add_wires[0]), pyrtl.concat(*add_wires[1]))
-
-    # Concatenate the results, and then return them.
-    # Perhaps here we should slice off the overflow bit, if it exceeds bit_length?
-    # result = result[:-1]
-    if result is None:
-        result = adder_result
-    else:
-        result = pyrtl.concat(adder_result, result)
+    # now we need to add them up
+    result = _sparse_adder(wire_array_2, final_adder)
     if len(result) > result_bitwidth:
         return result[:result_bitwidth]
     else:
         return result
 
+
+def _sparse_adder(wire_array_2, adder):
+    bitwidth = len(wire_array_2)
+    add_wires = [], []
+    result = []
+    for single_w_index in range(bitwidth):
+        if len(wire_array_2[single_w_index]) == 2:  # Check if the two wire vectors overlap yet
+            break
+        result.append(wire_array_2[single_w_index][0])
+
+    for w_loc in range(single_w_index, bitwidth):
+        for i in range(2):
+            if len(wire_array_2[w_loc]) >= i + 1:
+                add_wires[i].insert(0, wire_array_2[w_loc][i])
+            else:
+                add_wires[i].insert(0, pyrtl.Const(0))
+
+    adder_result = adder(pyrtl.concat(*add_wires[0]), pyrtl.concat(*add_wires[1]))
+    return pyrtl.concat(adder_result, *reversed(result))
+
 """
 Some adders that utilize these tree reducers
-
 """
 
 
