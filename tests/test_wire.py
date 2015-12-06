81a9a8707366d47b0674622d0302d995b6f7053c
John Clow <johnwclow@hotmail.com>
2015-12-06 15:36:36 -0800

    Cleaned up and extended the test suite for test_wire

--- tests/test_wire.py
+++ tests/test_wire.py
@@ -1,5 +1,4 @@
 import unittest
-import random
 import pyrtl
 
 from .helperfunctions import *
@@ -10,6 +9,51 @@ class TestWireVector(unittest.TestCase):
         pyrtl.reset_working_block()
 
     def tearDown(self):
+        pyrtl.reset_working_block()
+
+    def test_basic_assignment(self):
+        x = pyrtl.WireVector(1)
+        y = pyrtl.WireVector(1)
+        y <<= x
+
+    def test_unset_bitwidth(self):
+        x = pyrtl.Input()
+        y = pyrtl.WireVector(1)
+        with self.assertRaises(pyrtl.PyrtlError):
+            y <<= x
+
+    def test_assign_to_value(self):
+        x = pyrtl.WireVector(1)
+        y = 1337
+        with self.assertRaises(TypeError):
+            y <<= x
+
+    def test_slice(self):
+        testmissing()
+
+    def test_zero_extend(self):
+        testmissing()
+
+    def test_sign_extend(self):
+        testmissing()
+
+    def test_truncating(self):
+        testmissing()
+
+
+class TestInput(unittest.TestCase):
+    def setUp(self):
+        pyrtl.reset_working_block()
+
+    def test_assignment(self):
+        x = pyrtl.WireVector(1)
+        y = pyrtl.Input(1)
+        with self.assertRaises(pyrtl.PyrtlError):
+            y <<= x
+
+
+class TestRegister(unittest.TestCase):
+    def setUp(self):
         pyrtl.reset_working_block()
 
     def test_assignment_small(self):
@@ -29,34 +73,19 @@ class TestWireVector(unittest.TestCase):
         c.next <<= pyrtl.Const(202)
 
     def test_register_assignment_direct(self):
+        c = pyrtl.Register(bitwidth=3)
         with self.assertRaises(pyrtl.PyrtlError):
-            c = pyrtl.Register(bitwidth=3)
             c.next = 1
 
     def test_register_assignment_not_next(self):
+        c = pyrtl.Register(bitwidth=3)
         with self.assertRaises(pyrtl.PyrtlError):
-            c = pyrtl.Register(bitwidth=3)
             c <<= 1
 
     def test_logic_operations(self):
+        c = pyrtl.Register(bitwidth=1)
         with self.assertRaises(pyrtl.PyrtlError):
-            c = pyrtl.Register(bitwidth=1)
             c.next <<= c or True
-
-    def test_unset_bitwidth(self):
-        with self.assertRaises(pyrtl.PyrtlError):
-            x = pyrtl.WireVector(1)
-            y = pyrtl.Input()
-            x <<= y
-
-    def test_slice(self):
-        testmissing()
-
-    def test_zero_extend(self):
-        testmissing()
-
-    def test_sign_extend(self):
-        testmissing()
 
 
 # -------------------------------------------------------------------
@@ -65,79 +94,75 @@ class TestConst(unittest.TestCase):
         pyrtl.reset_working_block()
 
     def test_const_integers(self):
-        c = pyrtl.Const(1)
-        self.assertTrue(c.val == 1)
-        self.assertTrue(c.bitwidth == 1)
-
-        c = pyrtl.Const(5)
-        self.assertTrue(c.val == 5)
-        self.assertTrue(c.bitwidth == 3)
-
-        c = pyrtl.Const(1, bitwidth=5)
-        self.assertTrue(c.val == 1)
-        self.assertTrue(c.bitwidth == 5)
+        self.check_const(1, 1, 1)
+        self.check_const(5, 5, 3)
+        self.check_const(1, 1, 5, bitwidth=5)
 
     def test_const_neg_integers(self):
-        c = pyrtl.Const(-1, bitwidth=5)
-        self.assertTrue(c.val == 0b11111)
-        self.assertTrue(c.bitwidth == 5)
+        self.check_const(-1, 0b11111, 5, bitwidth=5)
+        self.check_const(-2, 0b110, 3, bitwidth=3)
+        self.check_const(-5, 0b1011, 4, bitwidth=4)
 
-        c = pyrtl.Const(-2, bitwidth=3)
-        self.assertTrue(c.val == 0b110)
-        self.assertTrue(c.bitwidth == 3)
+    def test_const_too_big(self):
+        self.assert_bad_const(5, 2)
 
-        c = pyrtl.Const(-5, bitwidth=4)
-        self.assertTrue(c.val == 0b1011)
-        self.assertTrue(c.bitwidth == 4)
+    def test_invalid_bitwidth(self):
+        self.assert_bad_const(1, 0)
+        self.assert_bad_const(1, -1)
 
     def test_const_bad_neg_integers(self):
         # check that bitwidth is required
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, -4)
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, -4, 2)
+        self.assert_bad_const(-4)
+        self.assert_bad_const(-4, 2)
 
     def test_const_string(self):
-        c = pyrtl.Const("1'1")
-        self.assertTrue(c.val == 1)
-        self.assertTrue(c.bitwidth == 1)
-
-        c = pyrtl.Const("5'3")
-        self.assertTrue(c.val == 3)
-        self.assertTrue(c.bitwidth == 5)
-
-        c = pyrtl.Const("5'b11")
-        self.assertTrue(c.val == 3)
-        self.assertTrue(c.bitwidth == 5)
-
-        c = pyrtl.Const("16'xff")
-        self.assertTrue(c.val == 0xff)
-        self.assertTrue(c.bitwidth == 16)
-
-        c = pyrtl.Const("17'xff")
-        self.assertTrue(c.val == 0xff)
-        self.assertTrue(c.bitwidth == 17)
-
-        c = pyrtl.Const("5'b011")
-        self.assertTrue(c.val == 3)
-        self.assertTrue(c.bitwidth == 5)
+        self.check_const("1'1", 1, 1)
+        self.check_const("5'3", 3, 5)
+        self.check_const("5'b11", 3, 5)
+        self.check_const("16'xff", 0xff, 16)
+        self.check_const("17'xff", 0xff, 17)
+        self.check_const("5'b011", 3, 5)
 
     def test_const_badstring(self):
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "-1")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1bx")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1ba")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1'bx")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1'ba")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1'b10")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "1'-b10")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "-1'b10")
-        self.assertRaises(pyrtl.PyrtlError, pyrtl.Const, "5'b111111'")
+        self.assert_bad_const("1")
+        self.assert_bad_const("-1")
+        self.assert_bad_const("1bx")
+        self.assert_bad_const("1ba")
+        self.assert_bad_const("1'bx")
+        self.assert_bad_const("1'ba")
+        self.assert_bad_const("1'b10")
+        self.assert_bad_const("1'-b10")
+        self.assert_bad_const("-1'b10")
+        self.assert_bad_const("5'b111111'")
+
+    def test_const_assignment(self):
+        testmissing()
+
+    def check_const(self, val_in, expected_val, expected_bitwidth, **kargs):
+        c = pyrtl.Const(val_in, **kargs)
+        self.assertEqual(c.val, expected_val)
+        self.assertEqual(c.bitwidth, expected_bitwidth)
+
+    def assert_bad_const(self, *args, **kwargs):
+        with self.assertRaises(pyrtl.PyrtlError):
+            c = pyrtl.Const(*args, **kwargs)
+
+
+class TestOutput(unittest.TestCase):
+    def setUp(self):
+        pyrtl.reset_working_block()
+
+    @unittest.skip  # this is not expected to pass
+    def test_assign_output(self):
+        o = pyrtl.Output(1)
+        w = pyrtl.WireVector(1)
+        with self.assertRaises(pyrtl.PyrtlError):
+            w <<= o
+
 
 # -------------------------------------------------------------------
 class TestRTLAdderDesign(unittest.TestCase):
     def setUp(self):
-        pyrtl.reset_working_block()
-
-    def tearDown(self):
         pyrtl.reset_working_block()
 
     def test_complete_adders(self):
