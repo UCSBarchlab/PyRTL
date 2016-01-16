532d4b05f97a0194577e96c2d3aed24ff9f3f916
John Clow <johnwclow@hotmail.com>
2016-01-16 15:08:21 -0800

    Updating test_aes to reflect testing_utils move

--- tests/rtllib/test_aes.py
+++ tests/rtllib/test_aes.py
@@ -1,7 +1,7 @@
 import unittest
-import t_utils
+
 import pyrtl
-from pyrtl.rtllib import aes
+from pyrtl.rtllib import aes, testingutils
 
 
 class TestAES(unittest.TestCase):
@@ -21,7 +21,7 @@ class TestAES(unittest.TestCase):
 
         in_vals = [0x3e1c22c0b6fcbf768da85067f6170495, 0x2d6d7ef03f33e334093602dd5bfb12c7]
         true_result = [0x3e175076b61c04678dfc2295f6a8bfc0, 0x2dfb02343f6d12dd09337ec75b36e3f0]
-        calculated_result = t_utils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
+        calculated_result = testingutils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
         self.assertEqual(calculated_result, true_result)
 
     def test_inv_sub_bytes(self):
@@ -29,7 +29,7 @@ class TestAES(unittest.TestCase):
 
         in_vals = [0x3e175076b61c04678dfc2295f6a8bfc0, 0x2dfb02343f6d12dd09337ec75b36e3f0]
         true_result = [0xd1876c0f79c4300ab45594add66ff41f, 0xfa636a2825b339c940668a3157244d17]
-        calculated_result = t_utils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
+        calculated_result = testingutils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
         self.assertEqual(calculated_result, true_result)
 
     def test_inv_mix_columns(self):
@@ -37,7 +37,7 @@ class TestAES(unittest.TestCase):
 
         in_vals = [0xe9f74eec023020f61bf2ccf2353c21c7, 0xbaa03de7a1f9b56ed5512cba5f414d23]
         real_res = [0x54d990a16ba09ab596bbf40ea111702f, 0x3e1c22c0b6fcbf768da85067f6170495]
-        calculated_result = t_utils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
+        calculated_result = testingutils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
         self.assertEqual(calculated_result, real_res)
 
     @unittest.skip
@@ -47,7 +47,7 @@ class TestAES(unittest.TestCase):
 
         in_vals = [0xd1876c0f79c4300ab45594add66ff41f, 0xfa636a2825b339c940668a3157244d17]
         true_result = [0x3e175076b61c04678dfc2295f6a8bfc0, 0x2dfb02343f6d12dd09337ec75b36e3f0]
-        calculated_result = t_utils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
+        calculated_result = testingutils.sim_and_ret_out(self.out_vector, (self.in_vector,), (in_vals,))
         self.assertEqual(calculated_result, true_result)
 
     def test_aes_full(self):
@@ -57,7 +57,7 @@ class TestAES(unittest.TestCase):
         ciphers = [0x3ad77bb40d7a3660a89ecaf32466ef97, 0x66e94bd4ef8a2c3b884cfa59ca342b2e]
         keys = [0x2b7e151628aed2a6abf7158809cf4f3c, 0x0]
         plain_text = [0x6bc1bee22e409f96e93d7e117393172a, 0x0]
-        calculated_result = t_utils.sim_and_ret_out(self.out_vector, (self.in_vector, aes_key),
+        calculated_result = testingutils.sim_and_ret_out(self.out_vector, (self.in_vector, aes_key),
                                                     (ciphers, keys))
         self.assertEqual(calculated_result, plain_text)
 
