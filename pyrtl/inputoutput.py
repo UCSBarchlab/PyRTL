a3f80e3d94304d0cda1d5846b6c8bcf3b1e8444b
nvandervoort <vandervoortn@gmail.com>
2016-07-22 11:19:20 -0700

    Added unused wires net_graph test and cleaned code for issue #204

--- pyrtl/inputoutput.py
+++ pyrtl/inputoutput.py
@@ -267,7 +267,7 @@ def net_graph(block=None, split_state=False):
             graph[w] = {}
 
     # add all of the edges
-    for w in block.wirevector_set.intersection(arg_set, dest_set):
+    for w in (dest_set & arg_set):
         try:
             _from = wire_src_dict[w]
         except Exception:
