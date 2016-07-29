261e220ed33d86fa5c47a8260ee3664cc759f4e5
nvandervoort <vandervoortn@gmail.com>
2016-07-28 17:49:19 -0700

    Added unit tests for sanity_check_net()

--- tests/test_core.py
+++ tests/test_core.py
@@ -20,20 +20,10 @@ class TestBlock(unittest.TestCase):
         with self.assertRaises(pyrtl.PyrtlError):
             pyrtl.working_block().add_wirevector(*args)
 
-    def invalid_net(self, *args):
-        with self.assertRaises(pyrtl.PyrtlInternalError):
-            pyrtl.working_block().add_net(*args)
-
     def test_add_wire(self):
         self.invalid_wire(None)
         self.invalid_wire("Hi John")
         self.invalid_wire(42)
-
-    def test_add_net(self):
-        self.invalid_net(None)
-        self.invalid_net(1)
-        self.invalid_net("hi")
-        self.invalid_net(pyrtl.Const(2))
 
     def test_undriven_net(self):
         w = pyrtl.WireVector(name='testwire', bitwidth=3)
@@ -94,6 +84,166 @@ class TestBlock(unittest.TestCase):
 
         for net in block.logic:
             print(net)
+
+
+class TestSanityCheckNet(unittest.TestCase):
+    def setUp(self):
+        pyrtl.reset_working_block()
+
+    def tearDown(self):
+        pyrtl.reset_working_block()
+
+    def invalid_net(self, exp_message, *args):
+        with self.assertRaisesRegexp(pyrtl.PyrtlInternalError, exp_message):
+            pyrtl.working_block().add_net(*args)
+
+    @staticmethod
+    def new_net(op='&', op_param=None, args=None, dests=None):
+        if args is None or isinstance(args, int):
+            args = tuple(pyrtl.Input(2) for i in range(args if isinstance(args, int) else 2))
+        if dests is None:
+            dests = (pyrtl.Output(2, 'out'),)
+        return pyrtl.LogicNet(op=op, op_param=op_param, args=args, dests=dests)
+
+    def test_net_make_with_not_net(self):
+        self.invalid_net("net must be of type LogicNet", None)
+        self.invalid_net("net must be of type LogicNet", 1)
+        self.invalid_net("net must be of type LogicNet", "hi")
+        self.invalid_net("net must be of type LogicNet", pyrtl.Const(2))
+
+    def test_net_no_tuples(self):
+        net = self.new_net(args=(pyrtl.Const(2)))
+        self.invalid_net("LogicNet args must be tuple", net)
+        net = self.new_net(dests=(pyrtl.Const(2)))
+        self.invalid_net("LogicNet dests must be tuple", net)
+
+    def test_net_odd_wires(self):
+        wire = pyrtl.WireVector(2, 'wire')
+        net = self.new_net(args=(wire, wire))
+        other_block = pyrtl.Block()
+        wire._block = other_block
+        self.invalid_net("net references different block", net)
+
+        pyrtl.reset_working_block()
+        wire = pyrtl.WireVector(2, 'wire')
+        net = self.new_net(args=(wire,))
+        pyrtl.working_block().remove_wirevector(wire)
+        self.invalid_net("net with unknown source", net)
+
+    def test_net_wrong_types(self):
+        inp = pyrtl.Input(2)
+        const = pyrtl.Const(2)
+        outp = pyrtl.Output(2)
+        net = self.new_net(dests=(inp,))
+        self.invalid_net("Inputs, Consts cannot be destinations", net)
+        net = self.new_net(dests=(const,))
+        self.invalid_net("Inputs, Consts cannot be destinations", net)
+        net = self.new_net(args=(outp, outp))
+        self.invalid_net("Outputs cannot be arguments", net)
+
+        wrong_ops = ('%', '!', 'a', 'f', '<<', '>>', '&&', '||', '==')
+        for op in wrong_ops:
+            net = self.new_net(op=op)
+            self.invalid_net("not from acceptable set", net)
+
+    def test_net_wrong_num_args(self):
+        for op in 'w~rsm':
+            net = self.new_net(op=op)
+            self.invalid_net("op only allowed 1 argument", net)
+        for op in '&|^n+-*<>=':
+            net = self.new_net(op=op, args=3)
+            self.invalid_net("op only allowed 2 arguments", net)
+        for op in 'x@':
+            net = self.new_net(op=op, args=4)
+            self.invalid_net("op only allowed 3 arguments", net)
+
+    def test_net_wrong_bitwidth(self):
+        net = self.new_net(op='x', args=tuple(pyrtl.Input(i) for i in range(1, 4)))
+        self.invalid_net("args have mismatched bitwidths", net)
+        net = self.new_net(op='x', args=tuple(pyrtl.Input(2) for i in range(3)))
+        self.invalid_net("mux select must be a single bit", net)
+
+        for op in '&|^n+-*<>=':
+            net = self.new_net(op=op, args=(tuple(pyrtl.Input(i) for i in range(2, 4))))
+            self.invalid_net("args have mismatched bitwidths", net)
+
+        net = self.new_net(op='m', op_param=(1234, pyrtl.MemBlock(2, 3)), args=1)
+        self.invalid_net("mem addrwidth mismatch", net)
+
+        net = self.new_net(op='@', op_param=(1234, pyrtl.MemBlock(2, 2)),
+                           args=tuple(pyrtl.Input(i) for i in (4, 2, 1)))
+        self.invalid_net("mem addrwidth mismatch", net)
+        net = self.new_net(op='@', op_param=(1234, pyrtl.MemBlock(2, 2)),
+                           args=tuple(pyrtl.Input(i) for i in (2, 4, 1)))
+        self.invalid_net("mem bitwidth mismatch", net)
+        net = self.new_net(op='@', op_param=(1234, pyrtl.MemBlock(2, 2)), args=3)
+        self.invalid_net("mem write enable must be 1 bit", net)
+
+    def test_net_wrong_num_op_params(self):
+        for op in 'w~r':
+            net = self.new_net(op=op, op_param=('hi', 'how', 'r', 'u'), args=1)
+            self.invalid_net("op_param should be None", net)
+        for op in '&|^n+-*<>=c':
+            net = self.new_net(op=op, op_param=('hi', 'how', 'r', 'u'), args=2)
+            self.invalid_net("op_param should be None", net)
+        net = self.new_net(op='x', op_param=('hi', 'how', 'r', 'u'),
+                           args=tuple(pyrtl.Input(i) for i in (1, 2, 2)))
+        self.invalid_net("op_param should be None", net)
+
+    def test_net_wrong_op_param_select(self):
+        net = self.new_net(op='s', op_param='hi', args=1)
+        self.invalid_net("select op requires tuple op_param", net)
+        net = self.new_net(op='s', op_param=(-2,), args=1)
+        self.invalid_net("op_param out of bounds", net)
+        net = self.new_net(op='s', op_param=(10,), args=1)
+        self.invalid_net("op_param out of bounds", net)
+
+    def test_net_wrong_op_param_mem(self):
+        for op in 'm@':
+            net = self.new_net(op=op, op_param=[1234, pyrtl.MemBlock(1, 1)],
+                               args=tuple(pyrtl.Input(1) for i in range(1 if op == 'm' else 3)))
+            self.invalid_net("mem op requires tuple op_param", net)
+        for op in 'm@':
+            net = self.new_net(op=op, op_param=(1234, pyrtl.MemBlock(1, 1), 'hi'),
+                               args=tuple(pyrtl.Input(1) for i in range(1 if op == 'm' else 3)))
+            self.invalid_net("mem op requires 2 op_params in tuple", net)
+        for op in 'm@':
+            net = self.new_net(op=op, op_param=('hi', pyrtl.MemBlock(1, 1)),
+                               args=tuple(pyrtl.Input(1) for i in range(1 if op == 'm' else 3)))
+            self.invalid_net("mem op requires first operand as int", net)
+
+        class NotMem:  # so that some earlier mem tests can work properly
+            def __init__(self, bw=1, aw=1):
+                self.bitwidth, self.addrwidth = bw, aw
+
+        for op in 'm@':
+            net = self.new_net(op=op, op_param=(1234, NotMem()),
+                               args=tuple(pyrtl.Input(1) for i in range(1 if op == 'm' else 3)))
+            self.invalid_net("mem op requires second operand of a memory type", net)
+
+    def test_net_dest_wrong_bitwidth(self):
+        for op in 'w~&|^nr':
+            net = self.new_net(op=op, args=1 if op in 'w~r' else 2, dests=(pyrtl.Output(3),))
+            self.invalid_net("upper bits of destination unassigned", net)
+        for op in '<>=':
+            net = self.new_net(op=op, dests=(pyrtl.Output(2),))
+            self.invalid_net("destination should be of bitwidth=1", net)
+        for op in '+-':
+            net = self.new_net(op=op, dests=(pyrtl.Output(4),))
+            self.invalid_net("upper bits of destination unassigned", net)
+        net = self.new_net(op='*', dests=(pyrtl.Output(5),))
+        self.invalid_net("upper bits of destination unassigned", net)
+        net = self.new_net(op='x', args=tuple(pyrtl.Input(1) for i in range(3)))
+        self.invalid_net("upper bits of mux output undefined", net)
+        net = self.new_net(op='c', args=3, dests=(pyrtl.Output(7),))
+        self.invalid_net("upper bits of concat output undefined", net)
+        net = self.new_net(op='s', args=1, op_param=(1,))
+        self.invalid_net("upper bits of select output undefined", net)
+        net = self.new_net(op='m', op_param=(1234, pyrtl.MemBlock(3, 2)), args=1)
+        self.invalid_net("mem read dest bitwidth mismatch", net)
+        net = self.new_net(op='@', op_param=(1234, pyrtl.MemBlock(2, 2)),
+                           args=tuple(pyrtl.Input(i) for i in (2, 2, 1)))
+        self.invalid_net("mem write dest should be empty tuple", net)
 
 
 class TestSetWorkingBlock(unittest.TestCase):
