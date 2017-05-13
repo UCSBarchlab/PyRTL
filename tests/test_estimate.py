484ec5fd5df394e153850123e0d893746a36c279
timsherwood <sherwood@cs.ucsb.edu>
2017-05-12 23:34:15 -0700

    ROMs added to estimate.  Area is 1/10 of SRAM. fixes #222

--- tests/test_estimate.py
+++ tests/test_estimate.py
@@ -43,6 +43,35 @@ class TestAreaEstimate(unittest.TestCase):
         mem[1] <<= a
         self.assertEquals(estimate.area_estimation(), (0.00734386752, 0.01879779717361501))
 
+    def test_area_est_unchanged_with_rom(self):
+        a = pyrtl.Const(2, 8)
+        b = pyrtl.Const(85, 8)
+        zero = pyrtl.Const(0, 1)
+        reg = pyrtl.Register(8)
+        mem = pyrtl.RomBlock(8, 8, romdata=list(range(0,256)))
+        out = pyrtl.Output(8)
+        nota, aLSB, athenb, aORb, aANDb, aNANDb, \
+        aXORb, aequalsb, altb, agtb, aselectb, \
+        aplusb, bminusa, atimesb, memread = [pyrtl.Output() for i in range(15)]
+        out <<= zero
+        nota <<= ~a
+        aLSB <<= a[0]
+        athenb <<= pyrtl.concat(a, b)
+        aORb <<= a | b
+        aANDb <<= a & b
+        aNANDb <<= a.nand(b)
+        aXORb <<= a ^ b
+        aequalsb <<= a==b
+        altb <<= a < b
+        agtb <<= a > b
+        aselectb <<= pyrtl.select(zero, a, b)
+        reg.next <<= a
+        aplusb <<= a + b
+        bminusa <<= a - b
+        atimesb <<= a*b
+        memread <<= mem[reg]
+        self.assertEquals(estimate.area_estimation(), (0.00734386752, 0.001879779717361501))
+
 
 class TestTimingEstimate(unittest.TestCase):
 
