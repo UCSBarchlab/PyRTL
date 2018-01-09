83cddb47b0893ec9e60261aeaeda7bf5545d93d0
timsherwood <sherwood@cs.ucsb.edu>
2018-01-09 11:00:12 -0800

    added recursive packages to fix #230

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'pyrtl',
     version = '0.8.5', #VERSION
-    packages =  ['pyrtl'],
+    packages =  find_packages(),
     description = 'RTL-level Hardware Design and Simulation Toolkit',
     author =  'Timothy Sherwood, John Clow, and UCSBarchlab',
     author_email =  'sherwood@cs.ucsb.edu',
