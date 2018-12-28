7704f043f47692e8349dafa5769f22ca64cf8fa1
yyang01@ucsb.edu <yyang01@ucsb.edu>
2018-12-28 19:51:07 +0800

    Added Xoroshiro128+

--- pyrtl/rtllib/prngs.py
+++ pyrtl/rtllib/prngs.py
@@ -13,20 +13,37 @@
 
     # seed once at the beginning
     sim.step({'load': 1, 'req': 0})
-    while sim.value[ready] == 0:
+    while sim.value[ready] == 0:  # or loop 19 cycles
         sim.step({'load': 0, 'req': 0})
 
     sim.step({'load': 0, 'req': 1})
-    while sim.value[ready] == 0:
+    while sim.value[ready] == 0:  # or loop 2 cycles
         sim.step({'load': 0, 'req': 0})
 
     print(sim.inspect(rand))
     sim_trace.render_trace(symbol_len=45, segment_size=5)
 
+    # prng_xoroshiro128
+    load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
+    ready, rand = pyrtl.Output(1, 'ready'), pyrtl.Output(128, 'rand')
+    ready_out, rand_out = prngs.prng_xoroshiro128(128, load, req)
+    ready <<= ready_out
+    rand <<= rand_out
+    sim_trace = pyrtl.SimulationTrace()
+    sim = pyrtl.Simulation(tracer=sim_trace)
+
+    sim.step({'load': 1, 'req': 0})  # seed once at the beginning
+    sim.step({'load': 0, 'req': 1})
+    while sim.value[ready] == 0:  # or loop 2 cycles
+        sim.step({'load': 0, 'req': 0})
+
+    print(sim.inspect(rand))
+    sim_trace.render_trace(symbol_len=40, segment_size=1)
+
     # prng_lfsr
     load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
-    rand = pyrtl.Output(32, 'rand')
-    rand <<= prngs.prng_lfsr(32, load, req)
+    rand = pyrtl.Output(64, 'rand')
+    rand <<= prngs.prng_lfsr(64, load, req)
     sim_trace = pyrtl.SimulationTrace()
     sim = pyrtl.Simulation(tracer=sim_trace)
 
@@ -34,21 +51,21 @@
     sim.step({'load': 0, 'req': 1})
     sim.step({'load': 0, 'req': 0})
     print(sim.inspect(rand))
-    sim_trace.render_trace(symbol_len=30, segment_size=1)
+    sim_trace.render_trace(symbol_len=40, segment_size=1)
 
     # explicit seeding
-    seed =  pyrtl.Input(89, 'seed')
+    seed =  pyrtl.Input(127, 'seed')
     load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
     rand = pyrtl.Output(32, 'rand')
     rand <<= prngs.prng_lfsr(32, load, req, seed)
     sim_trace = pyrtl.SimulationTrace()
     sim = pyrtl.Simulation(tracer=sim_trace)
 
-    sim.step({'load': 1, 'req': 0, 'seed': 0x102030405060708090a0b0c})
-    sim.step({'load': 0, 'req': 1, 'seed': 0x102030405060708090a0b0c})
-    sim.step({'load': 0, 'req': 0, 'seed': 0x102030405060708090a0b0c})
+    sim.step({'load': 1, 'req': 0, 'seed': 0x102030405060708090a0b0c0d0e0f010})
+    sim.step({'load': 0, 'req': 1, 'seed': 0x102030405060708090a0b0c0d0e0f010})
+    sim.step({'load': 0, 'req': 0, 'seed': 0x102030405060708090a0b0c0d0e0f010})
     print(sim.inspect(rand))
-    sim_trace.render_trace(symbol_len=30, segment_size=1)
+    sim_trace.render_trace(symbol_len=40, segment_size=1)
 
 """
 
@@ -59,31 +76,31 @@ import pyrtl
 
 def prng_lfsr(bitwidth, load, req, seed=None):
     """
-    Builds a single-cycle PRNG using a 89 bits Fibonacci LFSR.
+    Builds a single-cycle PRNG using a 127 bits Fibonacci LFSR.
 
     :param bitwidth: the desired bitwidth of the random number
-    :param load: one bit signal to load the seed into prng
+    :param load: one bit signal to load the seed into the prng
     :param req: one bit signal to request a random number
-    :param seed: 89 bits WireVector, defaults to None (self-seeding)
+    :param seed: 127 bits WireVector, defaults to None (self-seeding),
+      refrain from self-seeding if reseeding at run time is required
     :return: register containing the random number with the given bitwidth
 
-    A fast PRNG that generates a random number using only one clock cycle.
-    Large bitwidth may require very big area, because a regular LFSR outputs only
-    one bit per cycle, unrolling the loop adds extra gates. Not cryptographically strong,
-    but can be used as a test pattern generator or anything that requires random patterns.
+    A very fast and compact PRNG that generates a random number using only one clock cycle.
+    Has a period of 2**127 - 1. Its linearity makes it statistically weak, but should be
+    good enough for noncryptographic purposes like test pattern generation or simulation.
     """
-    # Note: 89 bits is chosen because 89 is a mersenne prime, which makes the period
-    # of the LFSR maximized at 2**89 - 1 for any bitwidth of random numbers
+    # 127 bits is chosen because 127 is a mersenne prime, which makes the period of the
+    # LFSR maximized at 2**127 - 1 for any requested bitwidth
     if seed is None:
         import random
         cryptogen = random.SystemRandom()
-        seed = cryptogen.randrange(1, 2**89)  # seeds itself if no seed signal is given
+        seed = cryptogen.randrange(1, 2**127)  # seed itself if no seed signal is given
 
-    lfsr = pyrtl.Register(89 if bitwidth < 89 else bitwidth)
+    lfsr = pyrtl.Register(127 if bitwidth < 127 else bitwidth)
     # leap ahead by shifting the LFSR bitwidth times
     leap_ahead = lfsr
     for i in range(bitwidth):
-        leap_ahead = pyrtl.concat(leap_ahead, leap_ahead[50] ^ leap_ahead[88])
+        leap_ahead = pyrtl.concat(leap_ahead, leap_ahead[125] ^ leap_ahead[126])
 
     with pyrtl.conditional_assignment:
         with load:
@@ -93,44 +110,112 @@ def prng_lfsr(bitwidth, load, req, seed=None):
     return lfsr[:bitwidth]
 
 
+def prng_xoroshiro128(bitwidth, load, req, seed=None):
+    """
+    Builds a PRNG using the Xoroshiro128+ algorithm in hardware.
+
+    :param bitwidth: the desired bitwidth of the random numberf
+    :param load: one bit signal to load the seed into the prng
+    :param req: one bit signal to request a random number
+    :param seed: 128 bits WireVector, defaults to None (self-seeding),
+      refrain from self-seeding if reseeding at run time is required
+    :return ready, rand: ready is a one bit signal showing the random number has been
+      produced, rand is a register containing the random number with the given bitwidth
+
+    An efficient noncryptographic PRNG, has much smaller area than Trivium.
+    But it does require a 64-bit adder to compute the output, so it is a bit slower.
+    Has a period of 2**128 - 1. Passes most statistical tests. Outputs a 64-bit random
+    word each cycle, takes multiple cycles if more than 64 bits are requested, and MSBs
+    of the random words are returned if the bitwidth is not a multiple of 64.
+
+    See also http://xoroshiro.di.unimi.it/
+    """
+    from math import ceil, log
+    from pyrtl.rtllib import adders
+    from pyrtl.rtllib.libutils import _shifted_reg_next as shift  # for readability
+    if seed is None:
+        import random
+        cryptogen = random.SystemRandom()
+        seed = cryptogen.randrange(1, 2**128)  # seed itself if no seed signal is given
+    seed = pyrtl.as_wires(seed, 128)
+
+    s0, s1 = (pyrtl.Register(64) for i in range(2))
+    output = pyrtl.WireVector(64)
+    # update internal states by xoring, rotating, and shifting
+    _s1 = s0 ^ s1
+    s0_next = (shift(s0, 'l', 55) | shift(s0, 'r', 9)) ^ shift(_s1, 'l', 14) ^ _s1
+    s1_next = shift(_s1, 'l', 36) | shift(_s1, 'r', 28)
+    output <<= adders.kogge_stone(s0, s1)
+
+    gen_cycles = ceil(bitwidth / 64)
+    counter_bitwidth = ceil(log(gen_cycles, 2)) if gen_cycles > 1 else 1  # make sure it is nonzero
+    rand = pyrtl.Register(gen_cycles * 64)
+    counter = pyrtl.Register(counter_bitwidth, 'counter')
+    gen_done = counter == gen_cycles - 1
+    state = pyrtl.Register(1)
+    WAIT, GEN = (pyrtl.Const(x) for x in range(2))
+    with pyrtl.conditional_assignment:
+        with load:
+            s0.next |= seed[:64]
+            s1.next |= seed[64:]
+            state.next |= WAIT
+        with req:
+            counter.next |= 0
+            s0.next |= s0_next
+            s1.next |= s1_next
+            rand.next |= pyrtl.concat(rand, output)
+            state.next |= GEN
+        with state == GEN:
+            with ~gen_done:
+                counter.next |= counter + 1
+                s0.next |= s0_next
+                s1.next |= s1_next
+                rand.next |= pyrtl.concat(rand, output)
+
+    ready = ~load & ~req & (state == GEN) & gen_done
+    return ready, rand[-bitwidth:]  # return MSBs because LSBs are less random
+
+
 def prng_trivium(bitwidth, load, req, seed=None, bits_per_cycle=64):
     """
     Builds a cyptographically secure PRNG using the Trivium stream cipher.
 
     :param bitwidth: the desired bitwidth of the random number
-    :param load: one bit signal to load the seed into csprng
+    :param load: one bit signal to load the seed into the prng
     :param req: one bit signal to request a random number
-    :param seed: 160 bits WireVector (80 bits key + 80 bits IV), defaults to None (self-seeding)
-    :param bits_per_cycle: the number of output bits to generate in parallel each cycle
-      Up to 64 bits each cycle. Must be a common divisor of 1152 and the bitwidth.
+    :param seed: 160 bits WireVector (80 bits key + 80 bits IV), defaults to None (self-seeding),
+      refrain from self-seeding if reseeding at run time is needed
+    :param bits_per_cycle: the number of output bits to generate in parallel each cycle,
+      up to 64 bits, must be a power of two: either 1, 2, 4, 8, 16, 32, or 64
     :return ready, rand: ready is a one bit signal showing either the random number has
       been produced or the seed has been initialized, rand is a register containing the
-      random number
+      random number with the given bitwidth
 
-    Trivium has a seed initialization stage that discards the first weak 1152 bits after
-    each loading. Generation stage can take multiple cycles as well depending on the given
-    bitwidth and bits_per_cycle.
-    Has small gate area and superior speed compared to AES and other stream ciphers.
+    This prng uses Trivium's key stream as its random bits output.
+    Both seed and key stream are MSB first (the earliest bit is stored at the MSB).
+    Trivium has a seed initialization stage that discards the first weak 1152 output bits
+    after each loading. Generation stage can take multiple cycles as well depending on the
+    given bitwidth and bits_per_cycle.
+    Has smaller gate area and faster speed than AES-CTR and any other stream cipher.
     Passes all known statistical tests. Can be used to generate encryption keys or IVs.
+    Designed to securely generate up to 2**64 bits. If more than 2**64 bits is needed,
+    must reseed after each generation of 2**64 bits.
+
+    Trivium specifications:
+    http://www.ecrypt.eu.org/stream/ciphers/trivium/trivium.pdf
+    See also the eSTREAM portfolio page:
+    http://www.ecrypt.eu.org/stream/e2-trivium.html
     """
-    # Trivium specifications and performace referenced from:
-    # http://www.ecrypt.eu.org/stream/ciphers/trivium/trivium.pdf
-    # See also the eSTREAM portfolio page for Trivium:
-    # http://www.ecrypt.eu.org/stream/e2-trivium.html
     from math import ceil, log
-    if bits_per_cycle > 64:
-        raise pyrtl.PyrtlError('bits_per_cycle should not exceed 64')
-    if bitwidth % bits_per_cycle != 0 or 1152 % bits_per_cycle != 0:
+    if (64 // bits_per_cycle) * bits_per_cycle != 64:
         raise pyrtl.PyrtlError('bits_per_cycle is invalid')
-    if seed is None:  # seeds itself if no seed signal is given
+    if seed is None:
         import random
         cryptogen = random.SystemRandom()
-        key, iv = (cryptogen.randrange(2**80) for i in range(2))
-    else:
-        from pyrtl.rtllib import libutils
-        seed = pyrtl.as_wires(seed, 160)
-        iv, key = libutils.partition_wire(seed, 80)
-        # To use csprng as a stream cipher, put key in MSBs and IV in LSBs
+        seed = cryptogen.randrange(2**160)  # seed itself if no seed signal is given
+    seed = pyrtl.as_wires(seed, 160)
+    key = seed[80:]
+    iv = seed[:80]
 
     a = pyrtl.Register(93)
     b = pyrtl.Register(84)
@@ -144,14 +229,20 @@ def prng_trivium(bitwidth, load, req, seed=None, bits_per_cycle=64):
         feedback_b.append(t1 ^ a[90 - i] & a[91 - i] ^ b[77 - i])
         feedback_c.append(t2 ^ b[81 - i] & b[82 - i] ^ c[86 - i])
         output.append(t1 ^ t2 ^ t3)
+    # update internal states by shifting bits_per_cycle times
+    a_next = pyrtl.concat(a, *feedback_a)
+    b_next = pyrtl.concat(b, *feedback_b)
+    c_next = pyrtl.concat(c, *feedback_c)
 
+    init_cycles = 1152 // bits_per_cycle
+    gen_cycles = ceil(bitwidth / bits_per_cycle)
+    counter_bitwidth = ceil(log(max(init_cycles + 1, gen_cycles), 2))
     rand = pyrtl.Register(bitwidth)
-    state = pyrtl.Register(1)
-    counter_bitwidth = int(ceil(log(max(bitwidth, 1152) // bits_per_cycle + 1, 2)))
     counter = pyrtl.Register(counter_bitwidth, 'counter')
-    init_done = counter == 1152 // bits_per_cycle
-    gen_done = counter == bitwidth // bits_per_cycle
-    INIT, GEN = (pyrtl.Const(x) for x in range(2))
+    init_done = counter == init_cycles
+    gen_done = counter == gen_cycles - 1
+    state = pyrtl.Register(2)
+    WAIT, INIT, GEN = (pyrtl.Const(x) for x in range(3))
     with pyrtl.conditional_assignment:
         with load:
             counter.next |= 0
@@ -161,134 +252,24 @@ def prng_trivium(bitwidth, load, req, seed=None, bits_per_cycle=64):
             state.next |= INIT
         with req:
             counter.next |= 0
-            rand.next |= 0
+            a.next |= a_next
+            b.next |= b_next
+            c.next |= c_next
+            rand.next |= pyrtl.concat(rand, *output)
             state.next |= GEN
         with state == INIT:
             with ~init_done:
                 counter.next |= counter + 1
-                a.next |= pyrtl.concat(a, *feedback_a)
-                b.next |= pyrtl.concat(b, *feedback_b)
-                c.next |= pyrtl.concat(c, *feedback_c)
+                a.next |= a_next
+                b.next |= b_next
+                c.next |= c_next
         with state == GEN:
             with ~gen_done:
                 counter.next |= counter + 1
-                a.next |= pyrtl.concat(a, *feedback_a)
-                b.next |= pyrtl.concat(b, *feedback_b)
-                c.next |= pyrtl.concat(c, *feedback_c)
+                a.next |= a_next
+                b.next |= b_next
+                c.next |= c_next
                 rand.next |= pyrtl.concat(rand, *output)
 
-    ready = ~load & ~req & (state == INIT) & init_done | (state == GEN) & gen_done
+    ready = ~load & ~req & ((state == INIT) & init_done | (state == GEN) & gen_done)
     return ready, rand
-
-
-def fibonacci_lfsr(bitwidth, load, shift, seed):
-    """
-    Builds a generic LFSR configured in Fibonacci setting.
-
-    :param bitwidth: the bitwidth of the LFSR
-    :param load: one bit signal to load the seed into LFSR
-    :param shift: one bit signal to shift the LFSR
-    :param seed: bitwidth bits WireVector
-    :return: register containing the internal state of the LFSR
-      Entire state returned for flexibility. Take LSB only for maximum randomness.
-
-    A LFSR where the outputs of several bits are XORed to provide the input bit to be shifted in.
-    Generates a peudorandom bit (LSB) each cycle with a period of 2**bitwidth - 1.
-    """
-    if bitwidth not in lfsr_tap_table:
-        raise pyrtl.PyrtlError('Bitwidth {} is either invalid or not supported'.format(bitwidth))
-
-    lfsr = pyrtl.Register(bitwidth)
-    feedback = lfsr[lfsr_tap_table[bitwidth][0] - 1]
-    for tap in lfsr_tap_table[bitwidth][1:]:
-        feedback = feedback ^ lfsr[tap - 1]
-
-    with pyrtl.conditional_assignment:
-        with load:
-            lfsr.next |= seed
-        with shift:
-            lfsr.next |= pyrtl.concat(lfsr, feedback)
-    return lfsr
-
-
-def galois_lfsr(bitwidth, load, shift, seed):
-    """
-    Builds a generic LFSR configured in Galois setting.
-
-    :param bitwidth: the bitwidth of the LFSR
-    :param load: one bit signal to load the seed into LFSR
-    :param shift: one bit signal to shift the LFSR
-    :param seed: bitwidth bits WireVector or integer
-    :return: register containing the internal state of the LFSR
-      Entire state returned for flexibility. Take MSB only for maximum randomness.
-
-    A LFSR where the bit shifted out is XORed to the inputs of several bits.
-    Generates a peudorandom bit (MSB) each cycle with a period of 2**bitwidth - 1.
-    Faster than a Fibonacci LFSR. Outputs the same bit stream as a Fibonacci LFSR
-    with a time offset.
-    """
-    if bitwidth not in lfsr_tap_table:
-        raise pyrtl.PyrtlError('Bitwidth {} is either invalid or not supported'.format(bitwidth))
-
-    lfsr = pyrtl.Register(bitwidth)
-    shifted_lfsr = lfsr[-1]
-    for tap in reversed(range(1, bitwidth)):
-        if tap in lfsr_tap_table[bitwidth]:
-            # tap numbering is reversed for Galois LFSRs
-            shifted_lfsr = pyrtl.concat(lfsr[-1] ^ lfsr[bitwidth - tap - 1], shifted_lfsr)
-        else:
-            shifted_lfsr = pyrtl.concat(lfsr[bitwidth - tap - 1], shifted_lfsr)
-
-    with pyrtl.conditional_assignment:
-        with load:
-            lfsr.next |= seed
-        with shift:
-            lfsr.next |= shifted_lfsr
-    return lfsr
-
-
-# maximal-cycle taps taken from table by Roy Ward, Tim Molteno:
-# http://www.physics.otago.ac.nz/reports/electronics/ETR2012-1.pdf
-# taps for other number of bitwidth can be added in the same fashion
-lfsr_tap_table = {
-    2: (2, 1),
-    3: (3, 2),
-    4: (4, 3),
-    5: (5, 3),
-    6: (6, 5),
-    7: (7, 6),
-    8: (8, 6, 5, 4),
-    9: (9, 5),
-    10: (10, 7),
-    11: (11, 9),
-    12: (12, 11, 8, 6),
-    13: (13, 12, 10, 9),
-    14: (14, 13, 11, 9),
-    15: (15, 14),
-    16: (16, 14, 13, 11),
-    17: (17, 14),
-    18: (18, 11),
-    19: (19, 18, 17, 14),
-    20: (20, 17),
-    21: (21, 19),
-    22: (22, 21),
-    23: (23, 18),
-    24: (24, 23, 21, 20),
-    25: (25, 22),
-    26: (26, 25, 24, 20),
-    27: (27, 26, 25, 22),
-    28: (28, 25),
-    29: (29, 27),
-    30: (30, 29, 26, 24),
-    31: (31, 28),
-    32: (32, 30, 26, 25),
-    64: (64, 63, 61, 60),
-    89: (89, 51),
-    127: (127, 126),
-    128: (128, 127, 126, 121),
-    256: (256, 254, 251, 246),
-    521: (521, 489),
-    1024: (1024, 1015, 1002, 1001),
-    2048: (2048, 2035, 2034, 2029),
-    4096: (4096, 4095, 4081, 4069),
-}
--- tests/rtllib/test_prngs.py
+++ tests/rtllib/test_prngs.py
@@ -12,69 +12,98 @@ class TestPrngs(unittest.TestCase):
         pyrtl.reset_working_block()
 
     @staticmethod
-    def sw_fibonacci_lfsr(bitwidth, seed):
-        fib_lfsr = seed
+    def fibonacci_lfsr(seed):
+        lfsr = seed
         while True:
-            feedback = 0
-            for tap in prngs.lfsr_tap_table[bitwidth]:
-                feedback ^= fib_lfsr >> tap - 1
-            feedback = feedback & 1
-            fib_lfsr = fib_lfsr << 1 | feedback
-            yield feedback
+            bit = (lfsr >> 126 ^ lfsr >> 125) & 1
+            lfsr = lfsr << 1 | bit
+            yield bit
 
     @staticmethod
-    def sw_galois_lfsr(bitwidth, seed):
-        gal_lfsr = seed
+    def xoroshiro128plus(seed):
+        mask = 2**64 - 1
+        s = [seed & mask, seed >> 64]
         while True:
-            msb = gal_lfsr >> bitwidth - 1 & 1
-            gal_lfsr = gal_lfsr << 1
-            if msb:
-                for tap in prngs.lfsr_tap_table[bitwidth]:
-                    gal_lfsr ^= 1 << bitwidth - tap
-            yield msb
+            s0 = s[0]
+            s1 = s[1]
+            word = (s1 + s0) & mask
+            s1 ^= s0
+            s[0] = (s0 << 55 | s0 >> 9) ^ s1 ^ s1 << 14
+            s[1] = s1 << 36 | s1 >> 28
+            yield word
 
     def test_prng_lfsr(self):
-        seed = pyrtl.Input(89, 'seed')
+        seed = pyrtl.Input(127, 'seed')
         load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
         rand = pyrtl.Output(128, 'rand')
         rand <<= prngs.prng_lfsr(128, load, req, seed)
         sim_trace = pyrtl.SimulationTrace()
         sim = pyrtl.Simulation(tracer=sim_trace)
-        seed_vals = [random.randrange(1, 2**89) for i in range(5)]
+        in_vals = [random.randrange(1, 2**127) for i in range(5)]
 
         for trial in range(5):
-            sw_lfsr = TestPrngs.sw_fibonacci_lfsr(89, seed_vals[trial])
             true_val = 0
-            for bit in islice(sw_lfsr, 128):
+            for bit in islice(TestPrngs.fibonacci_lfsr(in_vals[trial]), 128):
                 true_val = true_val << 1 | bit
-            sim.step({'load': 1, 'req': 0, 'seed': seed_vals[trial]})
+            sim.step({'load': 1, 'req': 0, 'seed': in_vals[trial]})
             sim.step({'load': 0, 'req': 1, 'seed': 0x0})
             sim.step({'load': 0, 'req': 0, 'seed': 0x0})
             circuit_out = sim.inspect(rand)
             self.assertEqual(circuit_out, true_val,
-                             "\nAssertion failed on trial: {} Expected value: {} Gotten value: {}"
+                             "\nAssertion failed on trial {}\nExpected value: {}\nGotten value: {}"
                              .format(trial, hex(true_val), hex(circuit_out)))
+
+    def test_prng_xoroshiro128(self):
+        seed = pyrtl.Input(128, 'seed')
+        load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
+        ready = pyrtl.Output(1, 'ready')
+        rand = pyrtl.Output(128, 'rand')
+        ready_out, rand_out = prngs.prng_xoroshiro128(128, load, req, seed)
+        ready <<= ready_out
+        rand <<= rand_out
+        sim_trace = pyrtl.SimulationTrace()
+        sim = pyrtl.Simulation(tracer=sim_trace)
+        in_vals = [random.randrange(1, 2**128) for i in range(5)]
+
+        for trial in range(5):
+            true_val = 0
+            for word in islice(TestPrngs.xoroshiro128plus(in_vals[trial]), 2):
+                true_val = true_val << 64 | word
+            sim.step({'load': 1, 'req': 0, 'seed': in_vals[trial]})
+            sim.step({'load': 0, 'req': 1, 'seed': 0x0})
+            for cycle in range(2, 4):
+                sim.step({'load': 0, 'req': 0, 'seed': 0x0})
+            circuit_out = sim.inspect(rand)
+            self.assertEqual(circuit_out, true_val,
+                             "\nAssertion failed on trial {}\nExpected value: {}\nGotten value: {}"
+                             .format(trial, hex(true_val), hex(circuit_out)))
+
+        for ready_signal in sim_trace.trace['ready'][:3]:
+            self.assertEqual(ready_signal, 0)
+        self.assertEqual(sim_trace.trace['ready'][3], 1)
+        self.assertEqual(sim_trace.trace['ready'][4], 0)
 
     def test_prng_trivium(self):
         """
         Trivium test vectors retrived from:
         https://www.sheffield.ac.uk/polopoly_fs/1.12164!/file/eSCARGOt_full_datasheet_v1.3.pdf
+        bit ordering is modified to adapt to the Pyrtl implementation
         """
         in_vector = pyrtl.Input(160, 'in_vector')
         load, req = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'req')
         ready = pyrtl.Output(1, 'ready')
-        out_vector = pyrtl.Output(128, 'out_vector')
+        out_vector = pyrtl.Output(128, 'rand')
         ready_out, rand_out = prngs.prng_trivium(128, load, req, in_vector)
         ready <<= ready_out
         out_vector <<= rand_out
         sim_trace = pyrtl.SimulationTrace()
         sim = pyrtl.Simulation(tracer=sim_trace)
 
-        test_vectors = [0x0100000000000000000000000000000000000000,
-                        0x0a09080706050403020100000000000000000000,
-                        0xfffefdfcfbfaf9f8f7f600000000000000000000,
-                        0xfaa75401ae5b08b5620fc760f9922bc45df68f28,
-                        0xf5a24ffca95603b05d0abe57f08922bb54ed861f, ]
+        in_vals = [0x0100000000000000000000000000000000000000,
+                   0x0a09080706050403020100000000000000000000,
+                   0xfffefdfcfbfaf9f8f7f600000000000000000000,
+                   0xfaa75401ae5b08b5620fc760f9922bc45df68f28,
+                   0xf5a24ffca95603b05d0abe57f08922bb54ed861f, ]
 
         true_vals = [0x1cd761ffceb05e39f5b18f5c22042ab0,
                      0x372e6b86524afa71b5fee86d5cebb07d,
@@ -83,69 +112,22 @@ class TestPrngs(unittest.TestCase):
                      0xf142d1df4b36c7652cba2e4a22ee51a0, ]
 
         for trial in range(5):
-            sim.step({'load': 1, 'req': 0, 'in_vector': test_vectors[trial]})
+            sim.step({'load': 1, 'req': 0, 'in_vector': in_vals[trial]})
             for cycle in range(1, 20):
                 sim.step({'load': 0, 'req': 0, 'in_vector': 0x0})
             sim.step({'load': 0, 'req': 1, 'in_vector': 0x0})
-            for cycle in range(21, 24):
+            for cycle in range(21, 23):
                 sim.step({'load': 0, 'req': 0, 'in_vector': 0x0})
             circuit_out = sim.inspect(out_vector)
             self.assertEqual(circuit_out, true_vals[trial],
-                             "\nAssertion failed on trial: {} Expected value: {} Gotten value: {}"
+                             "\nAssertion failed on trial {}\nExpected value: {}\nGotten value: {}"
                              .format(trial, hex(true_vals[trial]), hex(circuit_out)))
 
         for ready_signal in sim_trace.trace['ready'][:19]:
             self.assertEqual(ready_signal, 0)
         self.assertEqual(sim_trace.trace['ready'][19], 1)
 
-        for ready_signal in sim_trace.trace['ready'][20:23]:
+        for ready_signal in sim_trace.trace['ready'][20:22]:
             self.assertEqual(ready_signal, 0)
-        self.assertEqual(sim_trace.trace['ready'][23], 1)
-
-    def test_fibonacci_lfsr(self):
-        bitwidth = random.choice(list(prngs.lfsr_tap_table.keys()))
-        seed = pyrtl.Input(bitwidth, 'seed')
-        load, shift = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'shift')
-        bit_out = pyrtl.Output(1, 'bit_out')
-        lfsr = prngs.fibonacci_lfsr(bitwidth, load, shift, seed)
-        bit_out <<= lfsr[0]
-        seed_vals = [random.randrange(1, 2**bitwidth) for i in range(5)]
-
-        for trial in range(5):
-            sim_trace = pyrtl.SimulationTrace()
-            sim = pyrtl.Simulation(tracer=sim_trace)
-            true_val = TestPrngs.sw_fibonacci_lfsr(bitwidth, seed_vals[trial])
-
-            sim.step({'load': 1, 'shift': 0, 'seed': seed_vals[trial]})
-            for cycle in range(1, 500):
-                sim.step({'load': 0, 'shift': 1, 'seed': 0x0})
-
-            for cycle in range(2, 500):
-                circuit_out = sim_trace.trace['bit_out'][cycle]
-                self.assertEqual(circuit_out, next(true_val),
-                                 "\nAssertion failed for bitwidth {} on trial {} on cycle {}"
-                                 .format(bitwidth, trial, cycle))
-
-    def test_galois_lfsr(self):
-        bitwidth = random.choice(list(prngs.lfsr_tap_table.keys()))
-        seed = pyrtl.Input(bitwidth, 'seed')
-        load, shift = pyrtl.Input(1, 'load'), pyrtl.Input(1, 'shift')
-        bit_out = pyrtl.Output(1, 'bit_out')
-        lfsr = prngs.galois_lfsr(bitwidth, load, shift, seed)
-        bit_out <<= lfsr[-1]
-        seed_vals = [random.randrange(1, 2**bitwidth) for i in range(5)]
-
-        for trial in range(5):
-            sim_trace = pyrtl.SimulationTrace()
-            sim = pyrtl.Simulation(tracer=sim_trace)
-            true_val = TestPrngs.sw_galois_lfsr(bitwidth, seed_vals[trial])
-
-            sim.step({'load': 1, 'shift': 0, 'seed': seed_vals[trial]})
-            for cycle in range(1, 500):
-                sim.step({'load': 0, 'shift': 1, 'seed': 0x0})
-
-            for cycle in range(1, 500):
-                circuit_out = sim_trace.trace['bit_out'][cycle]
-                self.assertEqual(circuit_out, next(true_val),
-                                 "\nAssertion failed for bitwidth {} on trial {} on cycle {}"
-                                 .format(bitwidth, trial, cycle))
+        self.assertEqual(sim_trace.trace['ready'][22], 1)
+        self.assertEqual(sim_trace.trace['ready'][23], 0)
