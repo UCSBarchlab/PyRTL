64fc49e89c5fb59924d465366940164e13d9a5bb
timsherwood <tim.sherwood@gmail.com>
2021-03-05 17:58:34 -0800

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ copyright = u'2014, Timothy Sherwood et al.'
 # built documents.
 #
 # The short X.Y version.
-version = '0.8'  #VERSION
+version = '0.9'  #VERSION
 # The full version, including alpha/beta/rc tags.
 release = '0.9.0' #VERSION
 
