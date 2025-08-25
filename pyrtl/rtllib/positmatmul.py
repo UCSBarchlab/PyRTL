5aa148c4dfaf683a7cec61410311d6a5da6e161c
arvindajaybharadwaj <arvindajayarts@gmail.com>
2025-08-26 00:15:48 +0530

    Add doctest examples for positmatmul.py

--- pyrtl/rtllib/positmatmul.py
+++ pyrtl/rtllib/positmatmul.py
@@ -1,12 +1,45 @@
 import pyrtl
 from pyrtl import PyrtlError
-from pyrtl.rtllib.matrix import Matrix
-from positadder import posit_add
-from positmul import posit_mul
+from pyrtl.rtllib.matrix import Matrix, matrix_wv_to_list
+from pyrtl.rtllib.positadder import posit_add
+from pyrtl.rtllib.positmul import posit_mul
 
 
 def posit_matmul(x: Matrix, y: Matrix, nbits: int, es: int) -> Matrix:
     """Performs matrix multiplication on posits.
+
+    .. doctest only::
+
+    >>> import pyrtl
+    >>> from pyrtl.rtllib.matrix import Matrix, matrix_wv_to_list
+    >>> pyrtl.reset_working_block()
+
+    Example::
+
+    >>> nbits = 8
+    >>> es = 1
+
+    >>> matrix_x = [[0b01000000, 0b01010000], [0b01011000, 0b01100000]]
+    >>> test_x = Matrix(2, 2, bits=nbits, value=matrix_x)
+
+    >>> matrix_y = [[0b01000000, 0b01010000], [0b01011000, 0b01100000]]
+    >>> test_y = Matrix(2, 2, bits=nbits, value=matrix_y)
+
+    >>> result = posit_matmul(test_x, test_y, nbits, es)
+
+    >>> output = pyrtl.Output(name='output')
+    >>> output <<= result.to_wirevector()
+
+    >>> sim = pyrtl.Simulation()
+    >>> sim.step()
+
+    >>> raw_matrix = matrix_wv_to_list(
+    ...     sim.inspect("output"), result.rows, result.columns, result.bits
+    ... )
+
+    >>> pretty_matrix = [[format(val, '08b') for val in row] for row in raw_matrix]
+    >>> pretty_matrix
+    [['01100110', '01101010'], ['01101111', '01110001']]
 
     :param x: A :class:`Matrix` to be multiplied.
     :param y: A :class:`Matrix` to be multiplied.
@@ -43,7 +76,7 @@ def posit_matmul(x: Matrix, y: Matrix, nbits: int, es: int) -> Matrix:
         for j in range(y.columns):
             acc = pyrtl.Const(0, bitwidth=nbits)
             for k in range(x.columns):
-                prod = posit_mul(nbits, es, x[i, k], y[k, j])
+                prod = posit_mul(x[i, k], y[k, j], nbits, es)
                 acc = posit_add(acc, prod, nbits, es)
             result[i, j] = acc
 
