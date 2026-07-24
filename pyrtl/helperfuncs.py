c665654b3cf18e8fe96338541610eb87f8d9e8ec
Jeremy Lau <30300826+fdxmw@users.noreply.github.com>
2026-07-24 16:54:00 -0700

    Update `val_to_signed_integer` example so it does proper signed addition.

--- pyrtl/helperfuncs.py
+++ pyrtl/helperfuncs.py
@@ -765,19 +765,18 @@ def val_to_signed_integer(value: int, bitwidth: int) -> int:
     ``val_to_signed_integer`` can also be used as an ``repr_func`` for
     :meth:`~SimulationTrace.render_trace`, to display signed integers in traces::
 
-        bitwidth = 3
-        counter = pyrtl.Register(name="counter", bitwidth=bitwidth)
-        counter.next <<= counter + 1
+        counter = pyrtl.Register(name="counter", bitwidth=3, reset_value=-4)
+        counter.next <<= pyrtl.signed_add(counter, 1)
 
         sim = pyrtl.Simulation()
-        sim.step_multiple(nsteps=2 ** bitwidth)
+        sim.step_multiple(nsteps=2 ** counter.bitwidth)
         sim.tracer.render_trace(repr_func=val_to_signed_integer)
 
     will display::
 
                │0 │1 │2 │3 │4 │5 │6 │7
 
-        counter ──┤1 │2 │3 │-4│-3│-2│-1
+        counter -4│-3│-2│-1├──┤1 │2 │3
 
     :func:`infer_val_and_bitwidth` performs the opposite conversion::
 
