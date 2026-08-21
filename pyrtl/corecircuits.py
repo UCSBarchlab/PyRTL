6b8795087c9c1a7cd9fea686657c027e1e36a750
Sijie Kong <sk3569@cornell.edu>
2026-08-21 10:03:56 -0400

    Add more accurate types

--- pyrtl/corecircuits.py
+++ pyrtl/corecircuits.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 import itertools
 
+from typing import Sequence, Iterable
+
 from pyrtl.conditional import otherwise
 from pyrtl.core import Block, LogicNet, working_block
 from pyrtl.pyrtlexceptions import PyrtlError, PyrtlInternalError
@@ -227,8 +229,7 @@ def concat(*args: WireVectorLike) -> WireVector:
     working_block().add_net(net)
     return outwire
 
-
-def concat_list(wire_list: list[WireVectorLike]) -> WireVector:
+def concat_list(wire_list: Sequence[WireVectorLike]) -> WireVector:
     """Concatenates a list of :class:`WireVectors<WireVector>` into a single
     :class:`WireVector`.
 
@@ -706,7 +707,7 @@ def shift_right_logical(
     return barrel.barrel_shifter(bits_to_shift, bit_in, dir, shift_amount)
 
 
-def match_bitwidth(*args: WireVector, signed: bool = False) -> tuple[WireVector]:
+def match_bitwidth(*args: WireVector, signed: bool = False) -> Iterable[WireVector, ...]:
     """Matches multiple :class:`WireVector` :attr:`bitwidths<~WireVector.bitwidth>` via
     zero- or sign-extension.
 
